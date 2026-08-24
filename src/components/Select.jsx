0da60830915c4ac870e81f9610d1c4243a2a9d13
Jose Angel <joseangel20@hotmail.es>
2026-08-24 19:29:04 -0400

    Se creo la paginas AcercaDe y Contacto

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,6 +1,6 @@
 export default function Select({ text }) {
   return (
-    <>
+    <div role ="group" aria-label={`Seleccionar ${text}`} className="select">
       <label htmlFor={text}>{text}:</label>
 
       <select id={text}>
@@ -9,6 +9,6 @@ export default function Select({ text }) {
         <option value="2">2</option>
         <option value="3">3</option>
       </select>
-    </>
+    </div>
   );
 }
