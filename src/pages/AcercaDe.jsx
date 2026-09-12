8eef0e06d980cfa380843ad1d05739449260f7c3
Jose Angel <joseangel20@hotmail.es>
2026-09-12 19:43:30 -0400

    fix rutas

--- src/pages/AcercaDe.jsx
+++ src/pages/AcercaDe.jsx
@@ -1,6 +1,6 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import AcercaDeMain from "../components/acercaDe/AcercaDeMain";
+import AcercaDeMain from "../components/AcercaDe/AcercaDeMain";
 export default function AcercaDe() {
   return (
     <>
