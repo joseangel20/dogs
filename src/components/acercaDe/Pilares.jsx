e4d610d9d3756a64765de97ac6ae9141fd1b638f
Jose Angel <joseangel20@hotmail.es>
2026-09-14 19:57:28 -0400

    Fix component path casing

--- src/components/acercaDe/Pilares.jsx
+++
@@ -1,8 +0,0 @@
-export default function Pilares({ pilaresData }) {
-  return pilaresData.map(({ title, description }) => (
-    <section className="pilar-item" key={title}>
-      <h3>{title}:</h3>
-      <p>{description}</p>
-    </section>
-  ));
-}
